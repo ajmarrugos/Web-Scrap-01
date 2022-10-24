e42912019603328f82913c6e3e9b6448663a6bff
ajmarrugos <ajmarrugos@gmail.com>
2022-10-24 14:32:51 -0500

    Command line interface

---
+++ cli/var_pack/msg.py
@@ -0,0 +1,41 @@
+# UI Messages
+bar = "----------------------------- "
+welcome = """ Hello! I'm your:
+              __               _        
+   |  _  |_  (_   _ ._ _. ._  |_) ._ _  
+ \_| (_) |_) __) (_ | (_| |_) |_) | (_) 
+                          |             """
+intro1 = "My purpose is to help you save time on your job seeking journey."
+intro2 = "I'll look into our 'available search pages' to get you the most offers as possible."
+
+# CLI Questions
+quest1 = "STEP 1: Please select one of our main pages to look into:"
+quest2 = "STEP 2: Please select the country where you want to work:"
+quest3 = "STEP 3: Please type the Position or Role you want to search for:"
+quest4 = "STEP 4: Please type the number of offers you want to get:"
+quest5 = "STEP 5: Please confirm if you want to proceed to make requests to the following URLs:"
+
+# CLI Ansewrs
+currency_format = "From now, salary results will be shown as: "
+search_for = "Excellent, just to confirm that we're going to search for: "
+got_url = "And make a request to the following URLs: "
+
+# CLI Feedback
+typed = "You typed: "
+opt_count = "You have: "
+not_typed = "You must type an option, please try again."
+selection = "You selected: "
+bad_selection = "Wrong input/option. Please try again..."
+too_much = "Your input must be less than 1000, please try again."
+working = "Processing..."
+got_html = "Ok... Here it comes..."
+total_pages = "Total number of pages to search is: "
+
+
+# Request Status
+did_get = "Content fetch succesful!"
+didnot_get = "Content fetch unsucessful."
+
+# Errors
+not_int = "Your input should be a Integer."
+not_str = "Your input should be a String."
